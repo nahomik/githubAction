8d9bf8e8cb71b671466bb7de649c95c9bae265c1
Nahom <117065834+nahomik@users.noreply.github.com>
2025-11-11 10:33:08 +0300

    Initialize Express app with a simple route

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hi!');
+});
+
+app.listen(3000, () => console.log('Running on :3000'));
+module.exports = app;
